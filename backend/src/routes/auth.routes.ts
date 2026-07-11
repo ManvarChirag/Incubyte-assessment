9ef0a8b48b961f5e8723999f81f371c3429a0478
Manvar Chirag <chiragmanvar0701@gmail.com>
2026-07-11 19:09:05 +0530

    feat: implement registration endpoint

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/register", (req, res) => {
+  return res.status(201).json({
+    message: "User registered successfully"
+  });
+});
+
+export default router;
